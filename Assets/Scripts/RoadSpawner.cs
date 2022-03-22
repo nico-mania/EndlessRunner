a9103d492cc93283ad0068f809d1824ea5b54665
nico-mania <55563178+nico-mania@users.noreply.github.com>
2022-03-22 18:07:48 +0100

    added Ground Shader changed road scale to 5x5 changed mats for URP

--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class RoadSpawner : MonoBehaviour
 {
     public List<GameObject> grounds;
-    private float offset = 300f;
+    private float offset = 50f;
 
 
     // Start is called before the first frame update
